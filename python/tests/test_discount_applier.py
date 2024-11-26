64a0445e25de986240cf40eea69ed810777b8ed6
CocoWantsACake <maxime27130@outlook.fr>
2024-11-26 16:44:10 +0100

    last

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -12,12 +12,6 @@ class Notifier:
         self.counter += 1
         self.user_list.append(user)
 
-"""
-class NotifierMock:
-    def __int__(self):
-        self.notify = ["a","b","c"]
-"""
-
 def test_apply_v1_no_mock():
     my_user_list = [User("a", "a"), User("b", "b"), User("c", "c")]
     notifier = Notifier()
@@ -40,21 +34,21 @@ def test_apply_v2_no_mock():
         assert my_user_list[i].email == notifier.user_list[i].email
 
 
-"""
+
 def test_apply_v1_with_mock():
     my_user_list = [User("a", "a"), User("b", "b"), User("c", "c")]
-    notifier = Notifier()
+    notifier = MagicMock()
 
     da = DiscountApplier(notifier)
     da.apply_v1(10, my_user_list)
 
-    assert notifier.counter == len(my_user_list)
+    assert notifier.notify.call_count == len(my_user_list)
 
-
+"""
 def test_apply_v2_with_mock():
     my_user_list = [User("a", "a"), User("b", "b"), User("c", "c")]
-    notifier = Notifier()
-
+    notifier =MagicMock
+    notifier.name = ["a","b","c"]
     da = DiscountApplier(notifier)
     da.apply_v2(10, my_user_list)
 
