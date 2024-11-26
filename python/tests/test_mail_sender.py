d00b691ae10a5737267f3501e451d932cd696bbd
CocoWantsACake <maxime27130@outlook.fr>
2024-11-26 15:24:31 +0100

    b

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -1,12 +1,40 @@
 import pytest
 
-from mail_sender import MailSender
+from mail_sender import MailSender, SendMailRequest, SendMailResponse
+from user import User
+
+class HttpClient:
+
+    user_email = None
+    request = None
+
+    def post(self, base_url, request):
+        self.request = request
+
+        if not isinstance(request, SendMailRequest):
+            raise Exception("'request' should be from the 'SendMailRequest' class, but is a '{}' isntead.".format(type(request)))
+
+        self.user_email = request.recipient
+        return SendMailResponse(503, "aa")
+
+#def create_base_scenario():
+
 
 def test_send_v1():
-    # TODO: write a test that fails due to the bug in MailSender.send_v1
-    pass
+    recipient = User("billy", "billy@efrei.net")
+    http_client = HttpClient()
+
+    mail_sender = MailSender(http_client)
+    mail_sender.send_v1(recipient, "blblblbllb")
+
+    assert http_client.user_email == recipient.email
 
 
 def test_send_v2():
-    # TODO: write a test that fails due to the bug in MailSender.send_v2
-    pass
+    recipient = User("billy", "billy@efrei.net")
+    http_client = HttpClient()
+
+    mail_sender = MailSender(http_client)
+    mail_sender.send_v2(recipient, "blblblbl")
+
+    assert isinstance(http_client.request, SendMailRequest)
